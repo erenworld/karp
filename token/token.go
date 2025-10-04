6bfc02e5792902a12d80c000490c1b72db0c4475
erenworld <erenxturkoglu@gmail.com>
2025-10-04 13:45:46 +0200

    Add helper function to check the keywords table for a given identifier

--- token/token.go
+++ token/token.go
@@ -35,3 +35,19 @@ const (
 	FUNCTION 	= "FUNCTION"
 	LET 		= "LET"
 )
+
+var keywords = map[string]TokenType{
+	"fn": FUNCTION,
+	"let": LET,
+}
+
+// Checks the keywords table to see whether the given identifier is in fact a keyword.
+// If it is, it returns the keyword’s TokenType constant. 
+// If it isn’t, we just get back token.IDENT, which is the TokenType for all user-defined identifiers.
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+
+	return IDENT
+}
