b43e4110f7a3bddbe9b3c7500a81148e26a9665d
erenworld <erenxturkoglu@gmail.com>
2025-10-04 14:42:55 +0200

    Add new tests

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -83,6 +83,34 @@ func TestNextToken(t *testing.T) {
 		{token.GT, ">"},
 		{token.INT, "5"},
 		{token.SEMICOLON, ";"},
+
+		{token.IF, "if"},
+		{token.LPAREN, "("},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.TRUE, "true"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.ELSE, "else"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.FALSE, "false"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+
+		// 10 == 10
+		{token.INT, "10"},
+		{token.EQ, "=="},
+		{token.INT, "10"},
+
+		// 10 != 9
+		{token.INT, "10"},
+		{token.NOT_EQ, "!="},
+		{token.INT, "9"},
 	
 		// Finally EOF
 		{token.EOF, ""},
