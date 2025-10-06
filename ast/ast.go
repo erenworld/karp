2a9434b50ee1dd74fc25f6e60e798d1a5ec26e45
erenworld <erenxturkoglu@gmail.com>
2025-10-06 12:08:14 +0200

    Add Parser and NextToken helper

--- ast/ast.go
+++ ast/ast.go
@@ -2,10 +2,17 @@ package ast
 
 // Recursive descent parser. And in particular, it’s a “top down operator precedence” parser
 // sometimes called “Pratt parser”, after its inventor Vaughan Pratt
-// let <identifier> = <expression>;
+
+import "github.com/erenworld/karp/token"
 
 type Node interface {
 	TokenLiteral() string
+}
+
+type LetStatement struct {
+	Token 	token.Token // the token.LET token
+	Name 	*Identifier
+	Value 	Expression
 }
 
 type Statement interface {
@@ -17,3 +24,25 @@ type Expression interface {
 	Node
 	expressionNode()
 }
+
+
+type Identifier struct {
+	Token token.Token // the token.IDENT token
+	Value string
+}
+
+type Program struct {
+	Statement []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statement) > 0 {
+		return p.Statement[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+func (i *Identifier) expressionNode() {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
