6ab8c7cb581e01cfb58e586fbc2bef6462295b12
erenworld <erenxturkoglu@gmail.com>
2025-10-17 07:46:56 +0200

    Extend our ast to function litterals

--- ast/ast.go
+++ ast/ast.go
@@ -5,6 +5,7 @@ package ast
 
 import (
 	"bytes"
+	"strings"
 
 	"karp/token"
 ) 
@@ -78,6 +79,12 @@ type IfExpression struct {
 type BlockStatement struct {
 	Token token.Token // the { token
 	Statements []Statement
+}
+
+type FunctionLiteral struct {
+	Token 		token.Token // fn token
+	Parameters	[]*Identifier
+	Body 		*BlockStatement
 }
 
 type Program struct {
@@ -216,6 +223,26 @@ func (bs *BlockStatement) String() string {
 	for _, s := range bs.Statements {
 		out.WriteString(s.String())
 	}
+
+	return out.String()
+}
+
+func (fl *FunctionLiteral) statementNode() {}
+func (fl *FunctionLiteral) TokenLiteral() string { return fl.Token.Literal }
+func (fl *FunctionLiteral) String() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") ")
+	out.WriteString(fl.Body.String())
+
 
 	return out.String()
 }
