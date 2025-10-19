76e2d57b3cfb9b71f865c844cda0ebc6b8cbe56e
erenworld <erenxturkoglu@gmail.com>
2025-10-19 16:19:40 +0200

    Add evaluation for integer

--- evaluator/eval.go
+++ evaluator/eval.go
@@ -7,9 +7,23 @@ import (
 
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
+	case *ast.Program:
+		return evalStatements(node.Statements)
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+	case *ast.ExpressionStatement: 
+		return Eval(node.Expression)
 	}
 
 	return nil
 } 
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, stmt := range stmts {
+		result = Eval(stmt)
+	}
+
+	return result
+}
