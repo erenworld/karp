85bb624f2e6ec2f559ae9bd4c9d0609c5e70ff0c
erenworld <erenxturkoglu@gmail.com>
2025-10-19 15:31:03 +0200

    Add error helper for karp

--- repl/repl.go
+++ repl/repl.go
@@ -36,7 +36,11 @@ func Start(in io.Reader, out io.Writer) {
 	}
 }
 
+
 func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, "Woops! We ran into some karp business here!\n")
+	io.WriteString(out, " parser errors:\n")
+	
 	for _, msg := range errors {
 		io.WriteString(out, "\t"+msg+"\n")
 	}
