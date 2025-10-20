f1eeef5018a91caf50e66a95d5a1163beee5ac74
erenworld <erenxturkoglu@gmail.com>
2025-10-20 20:41:25 +0200

    Move eval tests from parser_test to eval_test

--- evaluator/eval_test.go
+++ evaluator/eval_test.go
@@ -10,15 +10,40 @@ import (
 func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
-
 	program := p.ParseProgram()
-
 	return Eval(program)
 }
+
+func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
+	result, ok := obj.(*object.Integer)
+	if !ok {
+		t.Errorf("object is not Integer. got=%T (%+v)", obj, obj)
+		return false
+	}
+	if result.Value != expected {
+		t.Errorf("object has wrong value. got=%d, want=%d", result.Value, expected)
+		return false
+	}
+	return true
+}
+
+func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
+	result, ok := obj.(*object.Boolean)
+	if !ok {
+		t.Errorf("object is not Boolean. got=%T (%+v)", obj, obj)
+		return false
+	}
+	if result.Value != expected {
+		t.Errorf("object has wrong value. got=%t, want=%t", result.Value, expected)
+		return false
+	}
+	return true
+}
+
 func TestEvalIntegerExpression(t *testing.T) {
-	tests := []struct{
-		input 		string
-		expected 	int64
+	tests := []struct {
+		input    string
+		expected int64
 	}{
 		{"5", 5},
 		{"10", 10},
@@ -30,48 +55,64 @@ func TestEvalIntegerExpression(t *testing.T) {
 	}
 }
 
-func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
-	result, ok := obj.(*object.Integer)
-	if !ok {
-		t.Errorf("object is not Integer. got=%T (%+v)", obj, obj)
-		return false
-	}
-
-	if result.Value != expected {
-		t.Errorf("object has wrong value. got=%d, want=%d", result.Value, expected)
-		return false
-	}
-
-	return true
-}
-
 func TestEvalBooleanExpression(t *testing.T) {
-	tests := []struct{
-		input		string
-		expected 	bool
+	tests := []struct {
+		input    string
+		expected bool
 	}{
 		{"true", true},
 		{"false", false},
 	}
 
 	for _, tt := range tests {
-		evaluated := testEval(tt.input) 
+		evaluated := testEval(tt.input)
 		testBooleanObject(t, evaluated, tt.expected)
 	}
 }
 
-func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
-	result, ok := obj.(*object.Boolean)
-	if !ok {
-		t.Errorf("object is not Boolean. got=%T (%+v)", obj, obj)
-		return false
+func TestBangOperator(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"!true", false},
+		{"!false", true},
+		{"!5", false},
+		{"!!true", true},
+		{"!!false", false},
+		{"!!5", true},
 	}
 
-	if result.Value != expected {
-		t.Errorf("object has wrong value. got=%t, want=%t",
-		result.Value, expected)
-		return false
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
+}
+
+func TestEvalComplexIntegerExpressions(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"5", 5},
+		{"10", 10},
+		{"-5", -5},
+		{"-10", -10},
+		{"5 + 5 + 5 + 5 - 10", 10},
+		{"2 * 2 * 2 * 2 * 2", 32},
+		{"-50 + 100 + -50", 0},
+		{"5 * 2 + 10", 20},
+		{"5 + 2 * 10", 25},
+		{"20 + 2 * -10", 0},
+		{"50 / 2 * 2 + 10", 60},
+		{"2 * (5 + 10)", 30},
+		{"3 * 3 * 3 + 10", 37},
+		{"3 * (3 * 3) + 10", 37},
+		{"(5 + 10 * 2 + 15 / 3) * 2 + -10", 50},
 	}
 
-	return true
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testIntegerObject(t, evaluated, tt.expected)
+	}
 }
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"karp/ast"
 	"karp/lexer"
-	"karp/object"
 	"testing"
 )
 
@@ -720,56 +719,4 @@ func TestCallExpressionParsing(t *testing.T) {
 	testLiteralExpression(t, exp.Arguments[0], 1)
 	testInfixExpression(t, exp.Arguments[1], 2, "*", 3)
 	testInfixExpression(t, exp.Arguments[2], 4, "+", 5)
-}
-
-func TestBangOperator(t *testing.T) {
-	tests := []struct{
-		input		string
-		expected 	bool
-	}{
-		{"!true", false},
-		{"!false", true},
-		{"!5", false},
-		{"!!true", true},
-		{"!!false", false},
-		{"!!5", true},
-	}
-
-	for _, tt := range tests {
-		evaluated := testEval(tt.input)
-		testBooleanObject(t, evaluated, tt.expected)
-	}
-}
-
-func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
-	result, ok := obj.(*object.Boolean)
-	if !ok {
-		t.Errorf("object is not Boolean. got=%T (%+v)", obj, obj)
-		return false 
-	}
-
-	if result.Value != expected {
-		t.Errorf("object has wrong value. got=%t, want=%t",
-		result.Value, expected)
-		return false
-	}
-
-	return true
-}
-
-func TestEvalIntegerExpression(t *testing.T) {
-	tests := []struct{
-		input 		string 
-		expected 	int64
-	}{
-		{"5", 5},
-		{"10", 10},
-		{"-5", -5},
-		{"-10", -10},
-	}
-
-	for _, tt := range tests {
-		evaluated := testEval(tt.input)
-		testIntegerObject(t, evaluated, tt.expected)
-	}
 }
