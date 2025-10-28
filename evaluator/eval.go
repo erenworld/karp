5ae00d21a9eda8d92c085fd7f72d8672607a2547
erenworld <erenxturkoglu@gmail.com>
2025-10-28 09:27:58 +0100

    Feat: functions definitions and calls is now possible

--- evaluator/eval.go
+++ evaluator/eval.go
@@ -48,7 +48,7 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 			return val
 		}
 		return &object.ReturnValue{Value: val}
-	case *ast.LetStatement: // KEEP TRACK OF VALUES ?
+	case *ast.LetStatement:
 		val := Eval(node.Value, env)
 		if isError(val) {
 			return val
@@ -69,9 +69,44 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		if len(args) == 1 && isError(args[0]) {
 			return args[0]
 		}
+		return applyFunction(function, args)
 	}
 
 	return nil
+}
+
+// We convert fn to a *object.Function reference in order to access .Env and .Body fields
+func applyFunction(fn object.Object, args []object.Object) object.Object {
+	function, ok := fn.(*object.Function)
+	if !ok {
+		return newError("not a function: %s", fn.Type())
+	}
+
+	extendedEnv := extendFunctionEnv(function, args)
+	evaluated := Eval(function.Body, extendedEnv)
+
+	return unwrapReturnValue(evaluated)
+}
+
+func extendFunctionEnv(
+	fn *object.Function,
+	args []object.Object,
+) *object.Environment {
+	env := object.NewEnclosedEnvironment(fn.Env)
+
+	for paramIdx, param := range fn.Parameters {
+		env.Set(param.Value, args[paramIdx])
+	}
+
+	return env
+}
+
+func unwrapReturnValue(obj object.Object) object.Object {
+	if returnValue, ok := obj.(*object.ReturnValue); ok {
+		return returnValue.Value
+	}
+
+	return obj
 }
 
 func evalExpressions(
