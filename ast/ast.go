efa6523613659fb35926f9bf929b92c2aa6821b6
erenworld <erenxturkoglu@gmail.com>
2025-10-29 12:39:37 +0100

    Define AST node for array literals

--- ast/ast.go
+++ ast/ast.go
@@ -93,6 +93,11 @@ type CallExpression struct {
 	Arguments	[]Expression
 }
 
+type ArrayLiteral struct {
+	Token token.Token
+	Elements []Expression
+}
+
 type StringLiteral struct {
 	Token token.Token
 	Value string
@@ -280,3 +285,20 @@ func (ce *CallExpression) String() string {
 func (sl *StringLiteral) expressionNode() {}
 func (sl *StringLiteral) TokenLiteral() string { return sl.Token.Literal }
 func (sl *StringLiteral) String() string { return sl.Token.Literal }
+
+func (al *ArrayLiteral) expressionNode() {}
+func (al *ArrayLiteral) TokenLiteral() string { return al.Token.Literal }
+func (al *ArrayLiteral) String() string { 
+	var out bytes.Buffer
+
+	elements := []string{}
+	for _, el := range al.Elements {
+		elements = append(elements, el.String())
+	}
+
+	out.WriteString("[")
+	out.WriteString(strings.Join(elements, ", "))
+	out.WriteString("]")
+
+	return out.String()
+}
