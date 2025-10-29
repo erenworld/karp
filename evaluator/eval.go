a7bdc08726e5ceea8d5b2f7ba4b98557145ae294
erenworld <erenxturkoglu@gmail.com>
2025-10-29 10:59:19 +0100

    Added complete len() builtin to Karp language

--- evaluator/eval.go
+++ evaluator/eval.go
@@ -79,15 +79,16 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 
 // We convert fn to a *object.Function reference in order to access .Env and .Body fields
 func applyFunction(fn object.Object, args []object.Object) object.Object {
-	function, ok := fn.(*object.Function)
-	if !ok {
-		return newError("not a function: %s", fn.Type())
+	switch fn := fn.(type) {
+	case *object.Function:
+		extendedEnv := extendFunctionEnv(fn, args)
+		evaluated := Eval(fn.Body, extendedEnv)
+		return unwrapReturnValue(evaluated)
+	case *object.Builtin:
+		return fn.Fn(args...)
+	default:
+			return newError("not a function: %s", fn.Type())
 	}
-
-	extendedEnv := extendFunctionEnv(function, args)
-	evaluated := Eval(function.Body, extendedEnv)
-
-	return unwrapReturnValue(evaluated)
 }
 
 func extendFunctionEnv(
@@ -132,12 +133,15 @@ func evalIdentifier(
 	node *ast.Identifier,
 	env *object.Environment,
 ) object.Object {
-	val, ok := env.Get(node.Value)
-	if !ok {
-		return newError("identifier not found: %s", node.Value)
+	if val, ok := env.Get(node.Value); ok {
+		return val
 	}
 
-	return val
+	if builtin, ok := builtins[node.Value]; ok {
+		return builtin
+	}
+
+	return newError("identifier not found: %s", node.Value)
 }
 
 func evalBlockStatements(block *ast.BlockStatement, env *object.Environment) object.Object {
