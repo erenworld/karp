fd05f9e111308908bfe27f63db7225dcabda572f
erenworld <erenxturkoglu@gmail.com>
2025-10-30 11:00:49 +0100

    Fix memory leak by checking the given index with arrayIndex

--- evaluator/eval_test.go
+++ evaluator/eval_test.go
@@ -395,3 +395,57 @@ func TestArrayLiteral(t *testing.T) {
 	testIntegerObject(t, result.Elements[1], 6)
 	testIntegerObject(t, result.Elements[2], 7)
 }
+
+func TestArrayIndexExpressions(t *testing.T) {
+	tests := []struct{
+		input 	string
+		expected interface{}
+	}{
+		{
+			"[1, 2, 3][0]",
+			1,
+		},
+		{
+			"[1, 2, 3][1]",
+			2,
+		},
+		{
+			"let i = 0; [1][i];",
+			1,
+		},
+		{
+			"[1, 2, 3][1 + 1];",
+			3,
+		},
+		{
+			"let myArray = [1, 2, 3]; myArray[2];",
+			3,
+		},
+		{
+			"let myArray = [1, 2, 3]; myArray[0] + myArray[1] + myArray[2];",
+			6,
+		},
+		{
+			"let myArray = [1, 2, 3]; let i = myArray[0]; myArray[i]",
+			2,
+		},
+		{
+			"[1, 2, 3][3]",
+			nil,
+		},
+		{
+			"[1, 2, 3][-1]",
+			nil,
+		},
+	}
+
+	for _, tt := range tests {
+		eval := testEval(tt.input)
+		integer, ok := tt.expected.(int)
+		if ok {
+			testIntegerObject(t, eval, int64(integer))
+		} else {
+			testNullObject(t, eval)
+		}
+	}
+}
