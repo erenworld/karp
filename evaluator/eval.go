ec6c97fea73642958b41b0060b88887d8e6cba47
erenworld <erenxturkoglu@gmail.com>
2025-10-30 21:43:53 +0100

    Add hash case branch to the switch statement in evalIndexExpression

--- evaluator/eval.go
+++ evaluator/eval.go
@@ -139,6 +139,8 @@ func evalIndexExpression(left, index object.Object) object.Object {
 	switch {
 	case left.Type() == object.ARRAY_OBJ && index.Type() == object.INTEGER_OBJ:
 		return evalIndexExpression(left, index)
+	case left.Type() == object.HASH_OBJ:
+		return evalHashIndexExpression(left, index)
 	default:
 		return newError("index operator not supported: %s", left.Type())
 	}
