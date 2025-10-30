39472df166bc4aba3081e65dcdf1c68c73eebb5f
erenworld <erenxturkoglu@gmail.com>
2025-10-30 11:05:29 +0100

    Add support for arrays to len() builtin

--- evaluator/builtin.go
+++ evaluator/builtin.go
@@ -11,7 +11,9 @@ var builtins = map[string]*object.Builtin{
 				return newError("wrong number of arguments. got=%d, want=1", len(args))
 			}
 
-			switch arg := args[0].(type) {
+			switch arg := args[0].(type) {	
+			case *object.Array:
+				return &object.Integer{Value: int64(len(arg.Elements))}
 			case *object.String:
 				return &object.Integer{Value: int64(len(arg.Value))}
 			default:
